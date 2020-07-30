02ac46fc14e74f4467afc252ce42ecf3867f112a
PB-DK <68913863+PB-DK@users.noreply.github.com>
2020-07-30 09:56:12 +0100

    Add files via upload

---
+++ Chapter 13/src/SelfReview/Transaction.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SelfReview;
+
+/**
+ *
+ * @author User
+ */
+public class Transaction {
+    
+    private int accountNumber;
+    
+    public Transaction() {
+        
+    }
+    
+    public void getAccountNumber() {
+        
+                
+    }
+    
+    public int execute() {
+        
+        
+    }   
+}
